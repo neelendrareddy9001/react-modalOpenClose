2472ac3536c1e67ea5e77e68a4650dd199474c4b
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-01-17 19:55:32 +0100

    react-modalOpenClose

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'rect';
+import './App.css';
+import Modal from './Components/Modal';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;

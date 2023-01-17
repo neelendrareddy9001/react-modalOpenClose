ea92cac8088b52df97f8c2b43c78bbe16fbe5ced
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-17 20:16:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 import Modal from './Components/Modal';
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div className="App">
-      
+      <h1>Hey, click on the button to open the modal.</h1>
+      <button
+        className="openModalBtn"
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Open
+      </button>
+
+      {modalOpen && <Modal setOpenModal={setModalOpen} />}
     </div>
   );
 }
